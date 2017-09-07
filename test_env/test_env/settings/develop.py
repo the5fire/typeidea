1bab187ec95ac25c4ec8b928707b55d40e079b4d
车乞 <cheqi@cheqideMacBook-Air.local>
2017-09-07 11:41:43 +0800

    separation environment

---
+++ test_env/test_env/settings/develop.py
@@ -0,0 +1,14 @@
+from .base_setting import *
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEBUG = True
