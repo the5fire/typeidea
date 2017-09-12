2bdcf97ac6543c17603d43af2d138b16c2c8da98
allenhu <thefivefire@gmail.com>
2017-09-12 23:48:56 +0800

    配置form和inlinemodel

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -5,12 +5,15 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
+from .adminforms import PostAdminForm
 from .models import Post, Category, Tag
 from typeidea.custom_site import custom_site
 
 
 @admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
     list_display = [
         'title', 'category', 'status_show', 'owner',
         'created_time', 'operator'
@@ -32,7 +35,12 @@ class PostAdmin(admin.ModelAdmin):
 
     fieldsets = (  # 跟fields互斥
         ('基础配置', {
-            'fields': (('category', 'title'), 'content')
+            'fields': (
+                ('category', 'title'),
+                'desc',
+                'status',
+                'content'
+            )
         }),
         ('高级配置', {
             'fields': ('tags', ),
@@ -48,9 +56,18 @@ class PostAdmin(admin.ModelAdmin):
     operator.short_description = '操作'
 
 
+class PostInlineAdmin(admin.StackedInline):
+    fields = ('title', 'status')
+    # extra = 2  # 控制额外多几个
+    max_num = 4
+    model = Post
+
+
 @admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        PostInlineAdmin,
+    ]
 
 
 @admin.register(Tag, site=custom_site)
---
+++ typeidea/blog/adminforms.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+
+from django import forms
+
+
+class PostAdminForm(forms.ModelForm):
+    status = forms.BooleanField(label="是否删除", required=True)  # TODO: 处理布尔类型为我们需要的字段
+    desc = forms.CharField(widget=forms.Textarea, label='摘要', required=False)
