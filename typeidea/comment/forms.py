a98a5e250e02e9a58a963937967245302b1f7e86
the5fire <thefivefire@gmail.com>
2017-11-04 12:08:29 +0800

    抽离评论block

--- typeidea/comment/forms.py
+++ typeidea/comment/forms.py
@@ -7,10 +7,34 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    nickname = forms.CharField(
+        label='昵称',
+        max_length=50,
+        widget=forms.widgets.Input(
+            attrs={'class': 'form-control', 'style': "width: 60%;"}
+        )
+    )
+    email = forms.CharField(
+        label='Email',
+        max_length=50,
+        widget=forms.widgets.EmailInput(
+            attrs={'class': 'form-control', 'style': "width: 60%;"}
+        )
+    )
+    website = forms.CharField(
+        label='网站',
+        max_length=100,
+        widget=forms.widgets.URLInput(
+            attrs={'class': 'form-control', 'style': "width: 60%;"}
+        )
+    )
+
     content = forms.CharField(
         label="内容",
         max_length=500,
-        widget=forms.widgets.Textarea(attrs={'rows': 6, 'cols': 60})
+        widget=forms.widgets.Textarea(
+            attrs={'rows': 6, 'cols': 60, 'class': 'form-control'}
+        )
     )
 
     def clean_content(self):
--- typeidea/config/views.py
+++ typeidea/config/views.py
@@ -5,9 +5,16 @@ from django.views.generic import ListView
 
 from .models import Link
 from blog.views import CommonMixin
+from comment.forms import CommentForm
 
 
 class LinkView(CommonMixin, ListView):
     queryset = Link.objects.filter(status=1)
     template_name = 'config/links.html'
     context_object_name = 'links'
+
+    def get_context_data(self, **kwargs):
+        kwargs.update({
+            'comment_form': CommentForm(),
+        })
+        return super(LinkView, self).get_context_data(**kwargs)
