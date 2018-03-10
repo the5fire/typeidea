8f263dc981f7eedfd70592479f0dbebb97b7bc5e
the5fire <thefivefire@gmail.com>
2018-03-10 11:29:17 +0800

    init project

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+
+from .base import *  # NOQA
+
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
