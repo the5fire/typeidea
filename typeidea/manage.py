2a803ddeca6357f86b6c276a9aa97e0c2702707c
the5fire <thefivefire@gmail.com>
2018-03-10 11:49:52 +0800

    修复遗漏拆分settings的配置

--- typeidea/manage.py
+++ typeidea/manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typeidea.settings")
+    profile = os.environ.get('TYPEIDEA_PROFILE', 'develop')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typeidea.settings.%s" % profile)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
