93b289e750753dc6565e8a526a89c8b249cd3ee0
the5fire <thefivefire@gmail.com>
2018-03-10 14:42:47 +0800

    配置Tag和Category的admin界面

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Category, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time')
+    fields = ('name', 'status', 'is_nav')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
