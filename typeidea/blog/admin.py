25a4d87dbe8dbf9fd0c261fa595f2db815760062
the5fire <thefivefire@gmail.com>
2018-03-11 13:49:50 +0800

    增加文章数量

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -10,12 +10,17 @@ from .models import Post, Category, Tag
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time')
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = '文章数量'
 
 
 @admin.register(Tag)
