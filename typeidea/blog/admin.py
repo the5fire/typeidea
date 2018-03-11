d4850428b8de5698a3c96c05650984d236817c37
the5fire <thefivefire@gmail.com>
2018-03-11 13:27:30 +0800

    增加post admin配置

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import Post, Category, Tag
 
@@ -23,4 +25,42 @@ class TagAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+        return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'operator'
+    ]
+    list_display_links = []
+
+    list_filter = ['category', ]
+    search_fields = ['title', 'category__name']
+    save_on_top = True
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    # 编辑页面
+    save_on_top = True
+
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
