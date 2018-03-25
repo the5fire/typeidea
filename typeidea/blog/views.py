23f1e07094002e04963c2dc78a6c9454b5fdd79e
the5fire <thefivefire@gmail.com>
2018-03-25 15:44:48 +0800

    简单展示model的数据到模板中

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 
+from .models import Post, Tag
+
 
 def post_list(request, category_id=None, tag_id=None):
-    return render(request, 'blog/list.html', context={'name': 'post_list'})
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    else:
+        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+        if category_id:
+            post_list = post_list.filter(category_id=category_id)
+
+    return render(request, 'blog/list.html', context={'post_list': post_list})
 
 
 def post_detail(request, post_id=None):
-    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+    return render(request, 'blog/detail.html', context={'post': post})
