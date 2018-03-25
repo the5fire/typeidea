2b29e8e5e63db97b4966eb8ca659ef8356578b1c
the5fire <thefivefire@gmail.com>
2018-03-25 12:36:13 +0800

    配置template的展示

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,14 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def post_list(request, category_id=None, tag_id=None):
-    content = 'post_list category_id={category_id}, tag_id={tag_id}'.format(
-        category_id=category_id,
-        tag_id=tag_id,
-    )
-
-    return HttpResponse(content)
+    return render(request, 'blog/list.html', context={'name': 'post_list'})
 
 
-def post_detail(request, post_id):
-    return HttpResponse('detail')
+def post_detail(request, post_id=None):
+    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'typeidea',
     'blog',
     'config',
     'comment',
