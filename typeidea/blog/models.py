6e39c340a41b4d8993ecbd98efa25a8d3227ad77
the5fire <thefivefire@gmail.com>
2018-03-25 16:32:54 +0800

    重构views代码，修复n+1问题

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -70,3 +70,27 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    @staticmethod
+    def get_by_tag(tag_id):
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            tag = None
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)\
+                .select_related('owner', 'category')
+        return post_list, tag
+
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            category = None
+            post_list = []
+        else:
+            post_list = category.post_set.filter(status=Post.STATUS_NORMAL)\
+                .select_related('owner', 'category')
+        return post_list, category
--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Post, Tag, Category
+from .models import Post
 
 
 def post_list(request, category_id=None, tag_id=None):
@@ -8,21 +8,12 @@ def post_list(request, category_id=None, tag_id=None):
     category = None
 
     if tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            post_list = []
-        else:
-            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+        post_list, tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_list, category = Post.get_by_category(category_id)
     else:
-        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
-        if category_id:
-            try:
-                category = Category.objects.get(id=category_id)
-            except Category.DoesNotExist:
-                category = None
-            else:
-                post_list = post_list.filter(category_id=category_id)
+        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)\
+            .select_related('owner', 'category')
 
     context = {
         'category': category,
