9cbb546d9ca9773ca948c2ea3dfd56ba66cfc8eb
the5fire <thefivefire@gmail.com>
2018-03-25 15:52:12 +0800

    区分首页、分类页、标签页

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-from .models import Post, Tag
+from .models import Post, Tag, Category
 
 
 def post_list(request, category_id=None, tag_id=None):
+    tag = None
+    category = None
+
     if tag_id:
         try:
             tag = Tag.objects.get(id=tag_id)
@@ -14,9 +17,19 @@ def post_list(request, category_id=None, tag_id=None):
     else:
         post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
         if category_id:
-            post_list = post_list.filter(category_id=category_id)
+            try:
+                category = Category.objects.get(id=category_id)
+            except Category.DoesNotExist:
+                category = None
+            else:
+                post_list = post_list.filter(category_id=category_id)
 
-    return render(request, 'blog/list.html', context={'post_list': post_list})
+    context = {
+        'category': category,
+        'tag': tag,
+        'post_list': post_list,
+    }
+    return render(request, 'blog/list.html', context=context)
 
 
 def post_detail(request, post_id=None):
