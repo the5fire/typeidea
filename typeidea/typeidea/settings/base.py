1828e93f344698f516d7e46786660980d739cacd
the5fire <thefivefire@gmail.com>
2018-04-08 23:26:03 +0800

    配置本地静态资源

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -125,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/themes', THEME, "static"),
+]
