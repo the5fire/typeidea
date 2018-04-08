eb9fab62c1e4f5d00ffb1d8c90823b919171cef4
the5fire <thefivefire@gmail.com>
2018-04-08 20:55:44 +0800

    改造为class based view

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,60 +1,81 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
 
 from config.models import SideBar
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': self.get_sidebars(),
+        })
+        context.update(self.get_navs())
+        return context
 
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)\
-            .select_related('owner', 'category')
+    def get_sidebars(self):
+        return SideBar.objects.filter(status=SideBar.STATUS_SHOW)
 
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': get_sidebars(),
-    }
-    context.update(get_navs())
-    return render(request, 'blog/list.html', context=context)
+    def get_navs(self):
+        categories = Category.objects.filter(status=Category.STATUS_NORMAL)
+        nav_categories = []
+        normal_categories = []
+        for cate in categories:
+            if cate.is_nav:
+                nav_categories.append(cate)
+            else:
+                normal_categories.append(cate)
 
-
-def post_detail(request, post_id=None):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
-
-    context = {
-        'post': post,
-        'sidebars': get_sidebars(),
-    }
-    context.update(get_navs())
-    return render(request, 'blog/detail.html', context=context)
+        return {
+            'navs': nav_categories,
+            'categories': normal_categories,
+        }
 
 
-def get_navs():
-    categories = Category.objects.filter(status=Category.STATUS_NORMAL)
-    nav_categories = []
-    normal_categories = []
-    for cate in categories:
-        if cate.is_nav:
-            nav_categories.append(cate)
-        else:
-            normal_categories.append(cate)
-
-    return {
-        'navs': nav_categories,
-        'categories': normal_categories,
-    }
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
 
 
-def get_sidebars():
-    return SideBar.objects.filter(status=SideBar.STATUS_SHOW)
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """ 重写querset，根据分类过滤 """
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+    def get_queryset(self):
+        """ 重写querset，根据标签过滤 """
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag__id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -1,15 +1,18 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from blog.views import post_list, post_detail
+from blog.views import (
+    IndexView, CategoryView, TagView,
+    PostDetailView,
+)
 from config.views import links
 from .custom_site import custom_site
 
 urlpatterns = [
-    url(r'^$', post_list, name='index'),
-    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
-    url(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
+    url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
     url(r'^links/$', links, name='links'),
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),
