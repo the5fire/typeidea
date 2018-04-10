1876ba8c7b049621189de9b210d2b8c385cecf8c
the5fire <thefivefire@gmail.com>
2018-04-10 20:59:28 +0800

    抽象评论功能为独立的tag

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -2,8 +2,6 @@ from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
 
-from comment.forms import CommentForm
-from comment.models import Comment
 from config.models import SideBar
 from .models import Post, Category, Tag
 
@@ -82,14 +80,6 @@ class PostDetailView(CommonViewMixin, DetailView):
     template_name = 'blog/detail.html'
     context_object_name = 'post'
     pk_url_kwarg = 'post_id'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            'comment_form': CommentForm,
-            'comment_list': Comment.get_by_target(self.request.path),
-        })
-        return context
 
 
 class SearchView(IndexView):
