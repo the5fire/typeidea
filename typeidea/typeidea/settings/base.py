db28758d86e7e91bae930051409bd0d1411dd323
the5fire <thefivefire@gmail.com>
2018-04-21 12:36:00 +0800

    配置自定义存储，配置水印

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -152,3 +152,5 @@ CKEDITOR_CONFIGS = {
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 CKEDITOR_UPLOAD_PATH = "article_images"
+
+DEFAULT_FILE_STORAGE = 'typeidea.storage.WatermarkStorage'
---
+++ typeidea/typeidea/storage.py
@@ -0,0 +1,40 @@
+from io import BytesIO
+
+from django.core.files.storage import FileSystemStorage
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+class WatermarkStorage(FileSystemStorage):
+    def save(self, name, content, max_length=None):
+        # 处理逻辑
+        if 'image' in content.content_type:
+            # 加水印
+            image = self.watermark_with_text(content, 'the5fire.com', 'red')
+            content = self.convert_image_to_file(image, name)
+
+        return super().save(name, content, max_length=max_length)
+
+    def convert_image_to_file(self, image, name):
+        temp = BytesIO()
+        image.save(temp, format='PNG')
+        file_size = len(temp.read())
+        temp.seek(0, 0)
+        return InMemoryUploadedFile(temp, None, name, 'image/png', file_size, None)
+
+    def watermark_with_text(self, file_obj, text, color, fontfamily="/Users/the5fire/Downloads/font1060/font1060/Sofia-Regular.otf"):
+        image = Image.open(file_obj).convert('RGBA')
+        imageWatermark = Image.new('RGBA', image.size, (255, 255, 255, 0))
+
+        draw = ImageDraw.Draw(imageWatermark)
+        width, height = image.size
+        margin = 10
+        font = ImageFont.truetype(fontfamily, int(height / 20))
+        textWidth, textHeight = draw.textsize(text, font)
+        x = (width - textWidth - margin) / 2
+        y = height - textHeight - margin
+
+        draw.text((x, y), text, color, font)
+
+        return Image.alpha_composite(image, imageWatermark)
