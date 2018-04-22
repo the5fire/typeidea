7960a27ac0893bcfa07694b3afa5c4e9d947efe0
the5fire <thefivefire@gmail.com>
2018-04-22 12:48:04 +0800

    使用viewset处理post接口

--- typeidea/blog/apis.py
+++ typeidea/blog/apis.py
@@ -1,18 +1,12 @@
-from rest_framework import generics
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
+# from rest_framework.permissions import IsAdminUser
+
 
 from .models import Post
 from .serializers import PostSerializer
 
 
-@api_view()
-def post_list(request):
-    posts = Post.objects.all()
-    post_serializers = PostSerializer(posts, many=True)
-    return Response(post_serializers.data)
-
-
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
+class PostViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
+    # permission_classes = [IsAdminUser]
