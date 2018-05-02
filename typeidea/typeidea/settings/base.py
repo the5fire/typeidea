0998c4c2520cb7145103e4cf2663eb69c072b464
the5fire <thefivefire@gmail.com>
2018-05-02 20:10:24 +0800

    调整模板组织结构

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -62,7 +62,7 @@ THEME = 'bootstrap'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/themes', THEME)],
+        'DIRS': [os.path.join(BASE_DIR, 'themes', THEME, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,5 +127,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/themes', THEME, "static"),
+    os.path.join(BASE_DIR, 'themes', THEME, "static"),
 ]
