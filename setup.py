d2fab1be15866f820adfdfb79338fbb96905372f
the5fire <thefivefire@gmail.com>
2018-05-11 19:21:34 +0800

    增加打包配置

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='typeidea',
+    version='0.1',
+    description='Blog System base on Django',
+    author='the5fire',
+    author_email='thefivefire@gmail.com',
+    url='https://www.the5fire.com',
+    license='MIT',
+    packages=find_packages('typeidea'),
+    package_dir={'': 'typeidea'},
+    # package_data={'': [    # 打包数据文件，方法一
+        # 'themes/*/*/*/*',  # 需要按目录层级匹配
+    # ]},
+    include_package_data=True,  # 方法二 配合 MANIFEST.in文件
+    install_requires=[
+        'django~=1.11',
+    ],
+    scripts=[
+        'typeidea/manage.py',
+    ],
+    entry_points={
+        'console_scripts': [
+            'typeidea_manage = manage:main',
+        ]
+    },
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Blog :: Django Blog',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+    ],
+
+)
