62f7c8df0a6c3c6555c9010fdc89b50d88afc04d
the5fire <thefivefire@gmail.com>
2018-06-24 12:23:21 +0800

    去掉过早的优化

--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -81,7 +81,7 @@ class SideBar(models.Model):
             result = self.content
         elif self.display_type == self.DISPLAY_LATEST:
             context = {
-                'posts': Post.latest_posts(with_related=False)
+                'posts': Post.latest_posts()
             }
             result = render_to_string('config/blocks/sidebar_posts.html', context)
         elif self.display_type == self.DISPLAY_HOT:
