0c37a5eadc2154a185b8cf5a9470a0b13b9c17c6
WCJ <583306676@qq.com>
2019-10-05 19:49:56 +0800

    向胡阳老师提交自己的课程作业3

--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -1,12 +1,44 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import Comment
-from typeidea.custom_site import custom_site
+from .adminform import CommentAdminForms
+from typeidea.custom_site import comment_admin_site
+from typeidea.custom_admin import BaseOwnerAdmin
 
-
-@admin.register(Comment, site=custom_site)
+@admin.register(Comment, site=comment_admin_site)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('target', 'nickname', 'content', 'website', 'created_time')
+    # 展示页面定制
+    list_display = [
+        'post', 'content', 'nickname_show',
+        'email', 'created_time', 'open_comment_website'
+    ]
+    list_filter = ['nickname', 'post', 'created_time']
+    search_fields = ['nickname', 'post']
+
+    date_hierarchy = 'created_time'
+
+    # 编辑页面定制
+    form = CommentAdminForms
+    save_on_top = False
+    save_on_bottom = True
+    fields = (
+        ('post', 'nickname'),
+        'email',
+        'content', 'websit'
+    )
+    
+
+    def open_comment_website(self, obj):
+        return format_html(
+            '<a href={}>跳转至评论页</a>',
+            reverse('comment_amdin:comment_comment_change', args=(obj.id,))
+            )
+    open_comment_website.short_description = '评论页'
+
+
+class CommentInlineAdmin(admin.TabularInline):
+    fields = ('nickname','content')
+    extra = 1
+    model = Comment
--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -1,41 +1,39 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Link(models.Model):
-    STATUS_NORMAL = 1
-    STATUS_DELETE = 0
-    STATUS_ITEMS = (
-        (STATUS_NORMAL, '正常'),
-        (STATUS_DELETE, '删除'),
+    STATUS_ITEM = (
+        (1, '正常'),
+        (2, '删除'),
     )
-    title = models.CharField(max_length=50, verbose_name="标题")
-    href = models.URLField(verbose_name="链接")  # 默认长度200
-    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    title = models.CharField(max_length=50, verbose_name='标题')
+    href = models.URLField(verbose_name='链接')
+    status = models.PositiveIntegerField(default=1, choices=STATUS_ITEM, verbose_name='状态')
     weight = models.PositiveIntegerField(default=1, choices=zip(range(1, 6), range(1, 6)),
-                                         verbose_name="权重",
-                                         help_text="权重高展示顺序靠前")
+                                        verbose_name='权重', 
+                                        help_text='权重越高展示顺序越靠前')
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
-    owner = models.ForeignKey(User, verbose_name="作者")
-    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
-
-    class Meta:
-        verbose_name = verbose_name_plural = "友链"
+    class Meta():
+        verbose_name = verbose_name_plural = '友链'
 
     def __str__(self):
         return self.title
 
+    def status_show(self):
+        return '当前状态{}'.format(self.status)
+    status_show.short_description = '状态'
+
 
 class SideBar(models.Model):
-    STATUS_SHOW = 1
-    STATUS_HIDE = 0
-    STATUS_ITEMS = (
-        (STATUS_SHOW, '展示'),
-        (STATUS_HIDE, '隐藏'),
+    STATUS_ITEM = (
+        (1, '正常'),
+        (2, '下线')
     )
+
     SIDE_TYPE = (
         (1, 'HTML'),
         (2, '最新文章'),
@@ -43,17 +41,21 @@ class SideBar(models.Model):
         (4, '最近评论'),
     )
     title = models.CharField(max_length=50, verbose_name="标题")
-    display_type = models.PositiveIntegerField(default=1, choices=SIDE_TYPE,
-                                               verbose_name="展示类型")
-    content = models.CharField(max_length=500, blank=True, verbose_name="内容",
-                               help_text="如果设置的不是HTML类型，可为空")
+    display_type = models.PositiveIntegerField(default=1, choices=SIDE_TYPE, verbose_name='展示类型')
+    content = models.CharField(max_length=500, blank=True, verbose_name='内容',
+                                 help_text='如果设置的不是HTML类型，可为空') 
 
-    status = models.PositiveIntegerField(default=STATUS_SHOW, choices=STATUS_ITEMS, verbose_name="状态")
-    owner = models.ForeignKey(User, verbose_name="作者")
-    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    status = models.PositiveIntegerField(default=1, choices=STATUS_ITEM, verbose_name='状态')  
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
-    class Meta:
-        verbose_name = verbose_name_plural = "侧边栏"
+    class Meta():
+        verbose_name = verbose_name_plural = '侧边栏'
 
+    def status_show(self):
+        return '当前状态{}'.format(self.status)
+    status_show.short_description = '状态'
+    
     def __str__(self):
         return self.title
